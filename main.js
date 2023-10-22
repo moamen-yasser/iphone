7329c1017d342426aa3e8be749d7def940f54fd6
moamen-yasser <mwmnyasr00@gmail.com>
2023-10-22 22:22:33 +0300

    iphone

---
+++ main.js
@@ -0,0 +1,10 @@
+let img=document.getElementById("img");
+let container=document.querySelector("body");
+
+function phone(phone){
+    img.src=phone;
+}
+
+function back(color){
+    container.style.backgroundColor=color;
+}
